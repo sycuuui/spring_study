6a8f42d6ec7d1b000a6f44a04bf1f3410a229c2b
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-12-18 15:09:22 +0900

    init

---
+++ src/main/java/com/example/spring/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,9 @@
+package com.example.spring.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class,args);
+    }
+}

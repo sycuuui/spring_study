5663070ab197a4403dff8f03de5f1dfe34e32b88
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-01-10 15:46:43 +0900

    fix : security 주석처리

--- src/main/java/com/example/spring/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/com/example/spring/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,12 +1,12 @@
-package com.example.spring.springbootdeveloper.config;
-
-import com.example.spring.springbootdeveloper.service.UserDetailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-
-@RequiredArgsConstructor
-@Configuration
-public class WebSecurityConfig {
-    private final UserDetailService userDetailService;
-
-}
+//package com.example.spring.springbootdeveloper.config;
+//
+//import com.example.spring.springbootdeveloper.service.UserDetailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebSecurityConfig {
+//    private final UserDetailService userDetailService;
+//
+//}
